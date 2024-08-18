8853bc4d80aa03ece50a87d73ae80e1fdcacee2c
unknow <138153020+ff6f8d68@users.noreply.github.com>
2024-08-18 01:58:29 -0700

    Delete Solar-Tweaks-main directory

--- Solar-Tweaks-main/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import store from './store';
-
-import Cache from './javascript/cache';
-export const cache = new Cache();
-
-createApp(App).use(store).mount('#app');
